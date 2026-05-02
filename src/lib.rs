8b1e4da9b33aa8413e76cebdaad89c45976a8f5d
Julius Brummack <133630819+jbrummack@users.noreply.github.com>
2026-05-02 11:57:31 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,3 @@ mod network_status;
 pub trait NwCallback: Send + Sync {
     fn on_status(&self, cb: NetworkStatus);
 }
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
