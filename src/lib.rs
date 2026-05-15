fd8564074a621df15cdad4c42b31ae8456882c86
Julius Brummack <133630819+jbrummack@users.noreply.github.com>
2026-05-15 11:30:10 +0200

    Add test to confirm runtime operation works

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,27 @@ mod network_status;
 pub trait NwCallback: Send + Sync {
     fn on_status(&self, cb: NetworkStatus);
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::mpsc;
+
+    use super::*;
+    struct CallbackDummy(mpsc::Sender<NetworkStatus>);
+    impl NwCallback for CallbackDummy {
+        fn on_status(&self, cb: NetworkStatus) {
+            let _ = self.0.send(cb);
+        }
+    }
+
+    #[test]
+    fn test_operation() {
+        let (sender, receiver) = mpsc::channel();
+        let callback = CallbackDummy(sender);
+        NetworkMonitor::start(Box::new(callback));
+        while let Ok(msg) = receiver.recv() {
+            println!("{msg:?}");
+            break;
+        }
+    }
+}
